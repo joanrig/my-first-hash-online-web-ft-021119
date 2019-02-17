02d22b47aeecd3853c1ec87f24af562421a4b2e6
Joan Lyness <joanrigdon@gmail.com>
2019-02-17 03:57:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,14 +31,12 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest[:muskets] = 2
-  shipping_manifest
-
-
 
   # add 4 gun powder to the shipping_manifest hash below
-
-
+  shipping_manifest[:gun_powder] = 4
 
   # return the shipping_manifest hash below
+  shipping_manifest
+
 
 end
